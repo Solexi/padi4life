7fe3d38d2c279c3f5d30aa14a00e5d43749cf763
Solexi <sophia.ezeh@pau.edu.ng>
2023-10-29 19:12:38 +0100

    Implemented Responsiveness on Mobile

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -68,7 +68,7 @@ const Home: React.FC = () => {
                 </Box>
                 <Flex
                     h={["340px", "500px"]}
-                    w={["380px","400px"]}
+                    w={["350px","400px"]}
                     mt={"80px"}
                     // mx={["4vw", ""]}
                     justify={["center", "center"]}
